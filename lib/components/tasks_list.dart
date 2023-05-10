838614c00cb9ae4ac4dff68896f958b5238a86fe
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-05-10 09:02:01 +0500

    both task title and checkbox made responsive

--- lib/components/tasks_list.dart
+++ lib/components/tasks_list.dart
@@ -15,15 +15,52 @@ class TasksList extends StatelessWidget {
   }
 }
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({Key? key}) : super(key: key);
+
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Make noise'),
-      trailing: Checkbox(value: false, onChanged: (value) {}),
+      title: Text(
+        'Make noise',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckBox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (checkboxState) {
+          setState(() {
+            isChecked = checkboxState!;
+          });
+        },
+      ),
     );
   }
 }
 
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox({
+    super.key,
+    required this.checkboxState,
+    required this.toggleCheckboxState,
+  });
+
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
+    );
+  }
+}
